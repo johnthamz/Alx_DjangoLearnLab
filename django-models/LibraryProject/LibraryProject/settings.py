093f272e0f4a4c96347374e326f07c97b9f2614e
johnthamz <johnthamz44@gmail.com>
2025-11-19 16:39:17 +0300

    Removed duplicates

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'bookshelf',
-    'LibraryProject.relationship_app',
+    
+    'relationship_app',
 ]
 
 MIDDLEWARE = [
