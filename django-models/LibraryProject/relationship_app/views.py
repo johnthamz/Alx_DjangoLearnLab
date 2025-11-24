ed403ece223f4b0b0976321c5e4828410c87fa8f
johnthamz <johnthamz44@gmail.com>
2025-11-24 20:25:38 +0300

    Task 2: fixed URLs and added FBV/CBV views with templates

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+# LibraryProject/relationship_app/views.py
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView
+from .models import Book, Library
 
-# Create your views here.
+# Function-based view: render list_books.html
+def list_books(request):
+    qs = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/list_books.html', {'books': qs})
+
+
+# Class-based view: Library Detail using DetailView and template
+class LibraryDetailView(DetailView):
+    model = Library
+    context_object_name = 'library'           # template will use {{ library }}
+    template_name = 'relationship_app/library_detail.html'
+    pk_url_kwarg = 'pk'                       # matches urls.py <int:pk>
+
