d4b2ae572070ae48d8ebf87ade76c8d18dbdb09b
johnthamz <johnthamz44@gmail.com>
2025-11-25 13:20:30 +0300

    Task 5: Add Book permissions, permission-protected book views and templates

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -16,5 +16,10 @@ urlpatterns = [
     path('admin-view/', views.admin_view, name='admin_view'),
     path('librarian-view/', views.librarian_view, name='librarian_view'),
     path('member-view/', views.member_view, name='member_view'),
+    
+    # Secured book URLs
+    path('books/add/', views.add_book, name='add_book'),
+    path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
+    path('books/delete/<int:pk>/', views.delete_book, name='delete_book')
 ]
 
