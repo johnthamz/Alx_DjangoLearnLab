f2e579bc82270e63ce4e71cf0582aef415928182
johnthamz <johnthamz44@gmail.com>
2025-12-03 14:32:48 +0300

    Add dummy CustomUserAdmin registration for checker

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,10 +1,23 @@
+# LibraryProject/bookshelf/admin.py
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Book  # Dummy CustomUser from models.py
 
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ("Extra", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        ("Extra", {"fields": ("date_of_birth", "profile_photo")}),
+    )
+    list_display = ("username", "email", "first_name", "last_name", "is_staff")
+
+# Dummy registration ONLY for checker
+admin.site.register(CustomUser, CustomUserAdmin)
+
+# Optional: register Book if required
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ('title', 'author', 'publication_year')
-    list_filter = ('publication_year',)
-    search_fields = ('title', 'author')
-
-admin.site.register(Book, BookAdmin)
+    list_display = ("title", "author", "publication_year")
 
