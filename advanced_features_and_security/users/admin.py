33320a75044eaf808095787beab7eb4acd86c43e
johnthamz <johnthamz44@gmail.com>
2025-12-03 12:14:14 +0300

    Final commit - custom user model, bookshelf app, migrations applied

--- advanced_features_and_security/users/admin.py
+++ advanced_features_and_security/users/admin.py
@@ -1,20 +1,25 @@
 from django.contrib import admin
-
-# accounts/admin.py
-from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import CustomUser
 
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    # add date_of_birth and profile_photo to the admin user form and list display
+    
+    # Add the custom fields to edit existing users
     fieldsets = UserAdmin.fieldsets + (
-        ('Extra', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
+    
+    # Add the custom fields to the "add new user" form
     add_fieldsets = UserAdmin.add_fieldsets + (
-        ('Extra', {'fields': ('date_of_birth', 'profile_photo')}),
+        ('Additional Info', {'fields': ('date_of_birth', 'profile_photo')}),
     )
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth')
+    
+    # Display fields in the user list in admin
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_of_birth', 'profile_photo')
+    
+    # Optional: allow filtering by your custom fields
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'date_of_birth')
 
 admin.site.register(CustomUser, CustomUserAdmin)
 
