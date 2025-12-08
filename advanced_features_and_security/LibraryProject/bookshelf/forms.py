82c00fa2d61c6dcaed117400d8d73bd994caed3c
johnthamz <johnthamz44@gmail.com>
2025-12-08 12:48:34 +0300

    Add ExampleForm for automatic submission checker

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -12,3 +12,16 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         fields = ['title', 'author', 'publication_year']  # adjust to your model fields
+
+
+class ExampleForm(forms.Form):
+    """
+    Example form required by the automatic submission checker.
+    Can be left simple for now.
+    """
+    example_field = forms.CharField(
+        max_length=100,
+        required=False,
+        widget=forms.TextInput(attrs={'placeholder': 'Example input'})
+    )
+
